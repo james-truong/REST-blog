a8d2e39791b08304b1b8150361b3e2bc4fcb5839
james-truong <james.truong@hotmail.com>
2021-04-16 18:25:48 -0400

    added authentication, statuses

--- routes/feed.js
+++ routes/feed.js
@@ -2,19 +2,21 @@ const express = require('express');
 const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
   '/post',
+  isAuth,
   [
     body('title')
       .trim()
-      .isLength({ min: 7 }),
+      .isLength({ min: 5 }),
     body('content')
       .trim()
       .isLength({ min: 5 })
@@ -22,6 +24,22 @@ router.post(
   feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
