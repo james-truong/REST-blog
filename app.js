7ca7b6b5f3510b02bda6c39273fb1a2290a25d2f
james-truong <james.truong@hotmail.com>
2021-05-15 17:57:54 -0400

    added tests for controllers using mocha and chai

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const graphqlHttp = require('express-graphql');
 
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
-const auth = require('./middleware/auth');
-const { clearImage } = require('./util/file');
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -48,46 +45,11 @@ app.use((req, res, next) => {
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
   next();
 });
 
-app.use(auth);
-
-app.put('/post-image', (req, res, next) => {
-  if (!req.isAuth) {
-    throw new Error('Not authenticated!');
-  }
-  if (!req.file) {
-    return res.status(200).json({ message: 'No file provided!' });
-  }
-  if (req.body.oldPath) {
-    clearImage(req.body.oldPath);
-  }
-  return res
-    .status(201)
-    .json({ message: 'File stored.', filePath: req.file.path });
-});
-
-app.use(
-  '/graphql',
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    formatError(err) {
-      if (!err.originalError) {
-        return err;
-      }
-      const data = err.originalError.data;
-      const message = err.message || 'An error occurred.';
-      const code = err.originalError.code || 500;
-      return { message: message, status: code, data: data };
-    }
-  })
-);
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -105,5 +67,3 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
-
-
